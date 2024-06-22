71248213d8c332654685947a5d647d6ae2362a8e
Rodrigo <rodrigopereda24@gmail.com>
2024-06-22 16:36:03 -0500

    creacion de entity

--- src/main/java/com/example/demo/Lp2SpringWebAplication.java
+++ src/main/java/com/example/demo/Lp2SpringWebAplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TemporalApplication {
+public class Lp2SpringWebAplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TemporalApplication.class, args);
+		SpringApplication.run(Lp2SpringWebAplication.class, args);
 	}
 
 }
